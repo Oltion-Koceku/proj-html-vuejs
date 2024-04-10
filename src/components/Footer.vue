d21e95496cd3d2da043ce348ba473153d28c157c
Oltion-Koceku <koceku17@gmail.com>
2024-04-10 20:35:53 +0200

    [add]footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,46 +1,75 @@
 <script>
-import { store } from '../data/store'
+import { store } from '../data/store';
 import { footerJs } from '../data/footerTopJs';
+import buttonvue from './partials/button.vue';
+
 export default {
   data() {
     return {
       store,
-      footerJs
-    }
+      footerJs,
+      footer: {},
+      text:"Book now"
+    };
   },
 
-  mounted(){
-    console.log(this.footerJs);
-  }
+  components: {
+    buttonvue
+  },
 
-}
+  methods: {
+    json() {
+      this.footer = this.footerJs[0].data; // Estrai i dati dal primo oggetto nell'array
+    },
+  },
+  mounted() {
+    this.json();
+    console.log(this.footer);
+  },
+};
 </script>
-
 
 <template>
   <div class="footer-top">
     <div class="container">
-      <div class="row row-cols-4">
-        <div v-for="(e, i) in footerJs.data" :key="i" class="col">
-          <div v-if="i === 0" class="drive">
-            <img :src="`src/img/${e.driving.url}`" alt="">
-            <p>{{ e.driving.text }}</p>
+      <div class="row row-cols-4 pt-5">
+        <div v-for="(e, i) in footer" :key="i" class="col">
+
+          <div v-if="e.id === 0" class="drive">
+            <img class="w-75 mb-2" :src="`src/img/${e.url}`" alt="">
+            <p>{{ e.text }}</p>
           </div>
-          <div v-if="i === 1" class="contact">
-            <div class="title">{{ e.contact.title }}</div>
+
+          <div v-if="e.id === 1" class="contact">
+            <div class="title">{{ e.title }}</div>
             <div class="info d-flex">
 
-              <div class="font"></div>
-              <div class="text"></div>
+              <div class="font mt-3">
+                <div v-for="(ico, i) in e.data" :key="i" v-html="ico.fontAwsome" class="icon"> </div>
+              </div>
+              <div class="text mt-3">
+                <div v-for="(ico, i) in e.data" :key="i">{{ ico.text }} </div>
+              </div>
 
 
             </div>
           </div>
-          <div v-if="i === 2" class="courses">
-            <div class="title"></div>
+          <div v-if="e.id === 2" class="courses">
+            <div class="title">{{ e.title }}</div>
             <div class="info d-flex">
-              <div class="font"></div>
-              <div class="text"></div>
+              <div class="font mt-3">
+                <div v-for="(cour, i) in e.textCourses" :key="i" v-html="e.fontAwsome" class="icon"> </div>
+              </div>
+              <div class="text mt-3">
+                <div v-for="(cour, i) in e.textCourses" :key="i" >{{ cour }} </div>
+              </div>
+            </div>
+          </div>
+
+          <div v-if="e.id === 3" class="img w-100 d-flex flex-column align-items-center justify-content-center">
+            <img class="w-100 mb-5" :src="`src/img${e.url}`" alt="">
+            <div class="butt  ">
+              <buttonvue :text = text />
             </div>
           </div>
         </div>
@@ -48,15 +77,79 @@ export default {
       </div>
     </div>
   </div>
-  <div class="footer-bottom"></div>
+  <div class="footer-bottom">
+    <div class="container d-flex align-items-center h-100 justify-content-between ">
+      <div class="text">
+        <p><i class="fa-regular fa-copyright"></i> Copyright 2012 - 2020 | Avada Theme by <span>ThemeFusion</span> | All Rights Reserved | Powered by <span>WordPress</span></p>
+      </div>
+      <div class="icon text-white">
+        <i class="fa-brands fa-facebook-f"></i>
+        <i class="fa-brands fa-twitter"></i>
+        <i class="fa-brands fa-youtube"></i>
+        <i class="fa-brands fa-instagram"></i>
+      </div>
+    </div>
+  </div>
 </template>
 
 
 
-<style lang="scss" scoped>
+
+
+
+<style scoped lang="scss">
+@use '../assets/scss/partials/_variables.scss' as *;
+
+
 .footer-top {
   height: 450px;
   background-image: url("../img/footer-background-color.jpg");
   background-size: cover;
+
+  .col {
+    color: $athensgray;
+    .courses{
+      .info{
+        color: $bayleaf;
+      }
+    }
+    .title {
+      font-size: 20px;
+      font-weight: 600;
+      color: white;
+    }
+
+    .icon {
+      height: 50px;
+      font-size: 18px;
+      margin-right: 15px;
+    }
+
+    .text {
+      div {
+        height: 50px;
+      }
+    }
+  }
+
+}
+
+.footer-bottom{
+  height: 150px;
+  background-color: $aztec;
+  color: $gumbo;
+  .text{
+    p{
+      span{
+        color: $bayleaf
+        ;
+      }
+    }
+  }
+  .icon{
+    i{
+      margin-left: 25px;
+    }
+  }
 }
 </style>
--- src/data/footerTopJs.js
+++ src/data/footerTopJs.js
@@ -2,39 +2,45 @@ const footerJs = [
   {
     data :{
       driving:{
+        id:0,
         url:"avada-drivers-logo-2x-200x39.png",
         text:"Avada Driving School is the right choice for experienced drivers looking for a refresher course, or the brand new driver wanting to pass first time"
       },
 
       contact:{
         title:"CONTACT DETAILS",
-        house:{
-          fontAwsome:'<i class="fa-solid fa-house-chimney"></i>',
-          text:"12345 North Main Street, New York, NY 555555"
-        },
+        id:1,
+        data:{
+          house:{
+            fontAwsome:'<i class="fa-solid fa-house-chimney"></i>',
+            text:"12345 North Main Street, New York, NY 555555"
+          },
 
-        tel:{
-          fontAwsome: '<i class="fa-solid fa-phone-flip"></i>',
-          text:"1.800.55.6789"
-        },
+          tel:{
+            fontAwsome: '<i class="fa-solid fa-phone-flip"></i>',
+            text:"1.800.55.6789"
+          },
 
-        mail:{
-          fontAwsome: '<i class="fa-regular fa-envelope"></i>',
-          text:"info@your-domain.com"
-        },
+          mail:{
+            fontAwsome: '<i class="fa-regular fa-envelope"></i>',
+            text:"info@your-domain.com"
+          },
 
-        link:{
-          fontAwsome:'<i class="fa-solid fa-link"></i>',
-          text:"Theme-Fusion.com"
+          link:{
+            fontAwsome:'<i class="fa-solid fa-link"></i>',
+            text:"Theme-Fusion.com"
+          }
         }
+        
         
 
       },
 
       courses:{
+        id:2,
         title:"COURSES",
-        fontAwsome:'<i class="fa-solid fa-circle-arrow-right"></i>',
-        text:{
+        fontAwsome:'<i class="fa-regular fa-circle-right"></i>',
+        textCourses:{
           1: "Pass Plus",
           2: "Intensive Course",
           3: "Automatic",
@@ -43,8 +49,8 @@ const footerJs = [
       },
 
       img:{
-        url:"/footer-ad-grid.png",
-        button:"<buttonVue />"
+        id:3,
+        url:"/footer-ad-grid.png"
 
       }
     }
