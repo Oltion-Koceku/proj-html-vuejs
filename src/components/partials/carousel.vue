899e60f87941aa8f19982c6a0949637969a91c45
Oltion-Koceku <koceku17@gmail.com>
2024-04-10 15:48:38 +0200

    [add]slider

--- src/components/partials/carousel.vue
+++ src/components/partials/carousel.vue
@@ -1,37 +1,93 @@
 <script>
-export default {
+import { testimonial } from '../../data/testimonials';
 
+export default {
+  data() {
+    return {
+      testimonial,
+      counter: 0
+    }
+  },
+  methods: {
+    autoSlider() {
+      setInterval(() => {
+        if (this.counter < this.testimonial.length - 1) {
+          this.counter++
+        } else {
+          this.counter = 0
+        }
+      }, 3000);
+    }
+  },
+  mounted() {
+    this.autoSlider();
+  }
 }
 </script>
 
+
 <template>
-  <div id="carouselExampleCaptions" class="carousel slide">
-    <div class="carousel-indicators">
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
-        aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
-        aria-label="Slide 2"></button>
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
-        aria-label="Slide 3"></button>
-    </div>
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="" class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>First slide label</h5>
-          <p>Some representative placeholder content for the first slide.</p>
-        </div>
+  <div class="carousel  w-50 text-center mt-5">
+    <h2 class="mt-5">Testimonianze</h2>
+    <div v-for="(e, i) in testimonial" :class="{ 'active': counter === i, 'd-none': counter !== i }" :key="i"
+      class="text-img d-flex flex-column align-items-center text-center mt-3">
+      <p>{{ e.text1 }}</p>
+      <img class="mb-5 mt-5" :src="(`src/img/${e.url}`)" alt="">
+      <p>{{ e.text2 }}</p>
+      <h3 class="mt-3 mb-3">{{ e.name }}</h3>
+      <div class="indicator">
+        <span v-for="(item, index) in testimonial" :key="index" :class="{ active2: index === counter }"></span>
       </div>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+
+<style lang="scss" scoped>
+@use '../../assets/scss/partials/_variables.scss' as *;
+
+.carousel {
+  h3 {
+    color: $gumbo;
+    font-weight: 600;
+    font-size: 22px;
+  }
+
+  p {
+    font-size: 21px;
+    font-weight: 500;
+    color: $gumbo;
+  }
+
+  .active {
+    display: block;
+  }
+
+  .active2 {
+    display: block;
+    background-color: grey !important;
+  }
+
+  .text-img img {
+    width: 200px;
+
+  }
+
+  .indicator {
+    margin-top: 10px;
+
+    span {
+      display: inline-block;
+      width: 10px;
+      height: 10px;
+      border-radius: 50%;
+      margin: 0 5px;
+      border: 1px solid grey;
+      background-color: transparent;
+    }
+  }
+
+
+
+}
+</style>
--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -3,22 +3,32 @@ const testimonial = [
     name:"Sophia Jones",
     url:"/testimonial-sophia.png",
     text1:"Here's what our happy dricvers had to say about our services:",
-    text2:"Avada Driving School really helped build my confidance behinf the wheel and with driving in general, and they got me a first time pass!"
+    text2:"Avada Driving School really helped build my confidance behinf the wheel and with driving in general, and they got me a first time pass! Highly recommended."
   },
   {
     name:"Kelly",
-    url:"/testimonial-sophia.png"
+    url:"/testimonial-kelly.png",
+    text1:"Here's what our happy dricvers had to say about our services:",
+    text2:"Avada Driving School really helped build my confidance behinf the wheel and with driving in general, and they got me a first time pass! Highly recommended."
   },
   {
     name:"Kate",
-    url:"/testimonial-sophia.png"
+    url:"/testimonial-kate.png",
+    text1:"Here's what our happy dricvers had to say about our services:",
+    text2:"Avada Driving School really helped build my confidance behinf the wheel and with driving in general, and they got me a first time pass! Highly recommended."
   },
   {
     name:"Harold",
-    url:"/testimonial-sophia.png"
+    url:"/testimonial-harold.png",
+    text1:"Here's what our happy dricvers had to say about our services:",
+    text2:"Avada Driving School really helped build my confidance behinf the wheel and with driving in general, and they got me a first time pass! Highly recommended."
   },
   {
     name:"Grant",
-    url:"/testimonial-sophia.png"
+    url:"/testimonial-grant.png",
+    text1:"Here's what our happy dricvers had to say about our services:",
+    text2:"Avada Driving School really helped build my confidance behinf the wheel and with driving in general, and they got me a first time pass! Highly recommended."
   },
 ]
+
+export {testimonial}
